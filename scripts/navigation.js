f284a3b4ca8187b2aadd4939caae5f5e9e5790fb
Richard Aurélien <aurelien.r@hotmail.ch>
2017-12-20 08:49:31 +0100

    Creation of repository

---
+++ scripts/navigation.js
@@ -0,0 +1,18 @@
+function toTwitch() {
+  //window.location.hash = '#twitch';
+  document.querySelector('#twitch').scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+function toContact() {
+  //window.location.hash = '#contact';
+  document.querySelector('#contact').scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+function toFaq() {
+  //window.location.hash = '#faq';
+  document.querySelector('#faq').scrollIntoView({
+    behavior: 'smooth'
+  });
+}
