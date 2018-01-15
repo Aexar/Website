ca577cda9bf343b80920b4d88e9861151895bc8b
Richard Aurélien <aurelien.r@hotmail.ch>
2018-01-15 13:41:30 +0100

    tried different header

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,3 +1,9 @@
+const navbar = document.getElementById('aexar-navbar');
+const navbarBaseColor = 'is-dark';
+
+
+
+
 function toTwitch() {
   //window.location.hash = '#twitch';
   document.querySelector('#twitch').scrollIntoView({
@@ -16,3 +22,41 @@ function toFaq() {
     behavior: 'smooth'
   });
 }
+
+if (navbar) {
+	window.addEventListener('scroll', () => {
+        if (window.scrollY > 0) {
+            navbar.classList.add('is-freestanding');
+            navbar.classList.add(navbarBaseColor);
+        } else {
+            navbar.classList.remove('is-freestanding');
+            navbar.classList.remove(navbarBaseColor);
+        }
+    });
+}
+
+/*document.addEventListener('DOMContentLoaded', function () {
+
+    // Get all "navbar-burger" elements
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function ($el) {
+            $el.addEventListener('click', function () {
+
+                // Get the target from the "data-target" attribute
+                var target = $el.dataset.target;
+                var $target = document.getElementById(target);
+
+                // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+                $el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+            });
+        });
+    }
+
+});*/
